2826cc119d4c529d45eaec5e0cba8191ad46d1fd
Vinicius Congeski <viniciuscongelo@gmail.com>
2023-11-18 18:18:05 -0300

    Mais diferenças entre var e let

---
+++ aula30/index.js
@@ -0,0 +1,34 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco { ... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Vinicius'; // criando
+// var nome2 = 'Congeski'; // criando
+
+// if (verdadeira) {
+//     let nome = 'Felipe'; // criando
+//     var nome2 = 'Nunes'; // redeclarando
+
+//     if (verdadeira) {
+//         var nome2 = 'Panda'; // redeclarando
+//         let nome = 'Outra coisa';
+//     }
+// }
+
+// console.log(nome, nome2);
+
+// function falaOi () { 
+
+//     if (verdadeira) {
+//         let nome = 'Vinicius';
+//         var sobrenome = 'Congeski';
+//     }
+
+//     console.log(sobrenome);
+// }
+
+// falaOi();
+
+let sobrenome = 'Nunes';
+console.log(sobrenome);
